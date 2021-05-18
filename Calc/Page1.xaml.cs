c2aa7ac43e231e3dca0518462cdd517d3a981947
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-05-18 16:20:39 +0300

    Мобильное задание часть 2

---
+++ Calc/Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calc
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public void Calculator(float first, float second, float result)
+        {
+            res.Text ="Размер премии: " + first + "*" + second + "=" + result;
+         }
+    }
+}
